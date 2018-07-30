c93c628625c0508345f617ee47519dbe530e742e
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2018-07-30 20:47:49 +0300

    chore package .gitignore

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 export default () => {
   console.log('Welcome to the Brain Games!');
 
-  const meeting = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${meeting}!`);
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
