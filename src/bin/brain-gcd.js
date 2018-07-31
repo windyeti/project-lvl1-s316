c77941efba80942ba8c6881e96423056d3226057
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2018-07-31 15:15:11 +0300

    feature brain-gcd

---
+++ src/bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import gameGcd from '../games/gameGcd';
+
+gameGcd();
---
+++ src/games/gameGcd.js
@@ -0,0 +1,26 @@
+import { cons } from 'hexlet-pairs';
+
+import gameFlow from '../gameFlow';
+import generateRandomNumber from '../utils';
+
+const getMaxDivisor = (num1, num2) => {
+  const max = Math.max(num1, num2);
+  const min = Math.min(num1, num2);
+  for (let i = min; i > 0; i -= 1) {
+    if (max % i === 0 && min % i === 0) {
+      return i.toString();
+    }
+  }
+  return false;
+};
+
+const rule = 'Find the greatest common divisor of given numbers.';
+const getGameData = () => {
+  const num1 = generateRandomNumber(1, 100);
+  const num2 = generateRandomNumber(1, 100);
+  const question = `${num1} ${num2}`;
+  const rightAnswer = getMaxDivisor(num1, num2);
+  return cons(question, rightAnswer);
+};
+
+export default () => gameFlow(rule, getGameData);
