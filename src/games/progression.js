37a8f33f76addd10b7b02e938f3de9e6d83b4fdf
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2018-08-02 10:25:46 +0300

    chore progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,7 +5,7 @@ import generateRandomNumber from '../utils';
 
 const lengthProgression = 10;
 
-const makeProgression = (start, increment, lengthProgression) => {
+const makeProgression = (start, increment) => {
   const progression = [];
   for (let i = 0; i < lengthProgression; i += 1) {
     progression.push(start + i * increment);
@@ -17,7 +17,7 @@ const rule = 'What number is missing in this progression?';
 const getGameData = () => {
   const start = generateRandomNumber(1, 10);
   const increment = generateRandomNumber(1, 10);
-  const progression = makeProgression(start, increment, lengthProgression);
+  const progression = makeProgression(start, increment);
   const indexRandom = generateRandomNumber(1, 8);
   const rightAnswer = progression[indexRandom];
   progression[indexRandom] = '..';
