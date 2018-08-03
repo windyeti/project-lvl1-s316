b59d3ec14f4d190fd07dee06607f36c58e44f3f2
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2018-08-03 15:53:19 +0300

    chore prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,28 +4,24 @@ import gameFlow from '../gameFlow';
 import generateRandomNumber from '../utils';
 
 const isPrime = (num) => {
-  for (let i = 2; i < num / 2; i += 1) {
+  if (num < 3) {
+    return -1;
+  } else if (num === 3) {
+    return num;
+  }
+  for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) {
-      return false;
+      return i;
     }
   }
-  return true;
+  return num;
 };
 
 const rule = 'Is this number prime?';
 
-const numMin = 2;
-const numMax = 500;
-
 const getGameData = () => {
-  let question = 0;
-  if (numMin < 2 || numMax < 2) {
-    console.log('Некорректно задан интервал возможных чисел');
-    question = generateRandomNumber(2, 100);
-  } else {
-    question = generateRandomNumber(numMin, numMax);
-  }
-  const rightAnswer = isPrime(question) ? 'yes' : 'no';
+  const question = generateRandomNumber(0, 4);
+  const rightAnswer = isPrime(question) === question ? 'yes' : 'no';
   return cons(question, rightAnswer);
 };
 
