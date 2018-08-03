8e6e73e51fa4745574e7d8fa4215d51dda271fa9
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2018-08-03 17:40:17 +0300

    chore prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,21 +4,21 @@ import gameFlow from '../gameFlow';
 import generateRandomNumber from '../utils';
 
 const isPrime = (num) => {
-  if (num < 3) return -1;
-  if (num === 3) return num;
+  if (num < 3) return false;
+  if (num === 3) return true;
   for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) {
-      return i;
+      return false;
     }
   }
-  return num;
+  return true;
 };
 
 const rule = 'Is this number prime?';
 
 const getGameData = () => {
   const question = generateRandomNumber(0, 10);
-  const rightAnswer = isPrime(question) === question ? 'yes' : 'no';
+  const rightAnswer = isPrime(question) ? 'yes' : 'no';
   return cons(question, rightAnswer);
 };
 
