dc1f7e69f2d81d873d1393eb1fc18b3d689503a1
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2018-08-04 11:50:29 +0300

    fix prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,8 +4,8 @@ import gameFlow from '../gameFlow';
 import generateRandomNumber from '../utils';
 
 const isPrime = (num) => {
-  if (num < 3) return false;
-  if (num === 3) return true;
+  if (num < 2) return false;
+  if (num === 2 || num === 3) return true;
   for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) {
       return false;
@@ -17,7 +17,7 @@ const isPrime = (num) => {
 const rule = 'Is this number prime?';
 
 const getGameData = () => {
-  const question = generateRandomNumber(0, 10);
+  const question = generateRandomNumber(0, 100);
   const rightAnswer = isPrime(question) ? 'yes' : 'no';
   return cons(question, rightAnswer);
 };
